ef3478181a52df6f99a0520f6cab7889a3ce5491
218492703 <218492703@tut4life.ac.za>
2024-06-23 04:55:38 +0200

    Add files via upload

---
+++ assign3.py
@@ -0,0 +1,30 @@
+# EXERCISE 1 30 MARKS
+# Assignment 3: Python
+# Student name: F Makhubela
+# Student no: 218492703
+# Date: 
+# Assignment 3: Pytho
+
+
+# Problem 1 - 10 Points#
+s= 'fullstackslp'
+print(s[0])
+print(s[11])
+print(s[4:9])
+print(s[9:12])
+print(s[7:10])
+print(s[::-1])
+
+## Problem 2 - LISTS - 5 Marks##
+d1 = {'simple_key':'hello'}
+print(d1['simple_key'])
+d2 = {'k1':{'k2':'hello'}}
+print(d2['k1']['k2'])
+d3 = {'k1':[{'nest_key':['this is deep',['hello']]}]}
+print(d3['k1'][0]['nest_key'][1][0])
+
+## Problem 4 - SETS - 4 Marks##
+age = 45
+name = "Kyle"
+dog_info= "Hello my dog's name is {} and he looks {} years old".format(name,age)
+print(dog_info)
